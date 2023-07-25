617d185e1c7f71fdc3e6b6046cd0866cca30824c
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2023-07-25 11:41:31 +0300

    Створення проекту
    Створення фінального проекту, підключення Firebase та створення компонентів хедера та футера

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'my-final-project-cbaf4',
+    appId: '1:990838836472:web:952e297f42e6e8ec16bf0d',
+    databaseURL: 'https://my-final-project-cbaf4-default-rtdb.firebaseio.com',
+    storageBucket: 'my-final-project-cbaf4.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCNPM7dybfRVXPebZ-nFbcYwyS0gWvlhCs',
+    authDomain: 'my-final-project-cbaf4.firebaseapp.com',
+    messagingSenderId: '990838836472',
+  },
+};
