7e1c241710fa18e4aa8f399f4db8d5c950ec978b
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2023-08-04 12:09:46 +0300

    Карусель
    Встановлено ngx-slick-carousel. Слайдер з акціями та стилі.

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionResponse } from 'src/app/shared/interfaces/action';
+import { ActionService } from 'src/app/shared/services/action/action.service';
 
 @Component({
   selector: 'app-home',
@@ -6,16 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  images: string[] = [];
+  constructor(private actionService: ActionService) {}
 
+  public slides: Array<ActionResponse> = [];
 
-  slickConfig = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  ngOnInit(): void {
+    this.getActions();
+  }
+
+  getActions(): void {
+    this.actionService.getAll().subscribe((data) => {
+      this.slides = data;
+      console.log(this.slides);
+    });
+  }
+
+  slideConfig = {
     slidesToShow: 1,
     slidesToScroll: 1,
-    // Тут ви можете додатково налаштувати слайдер
+    infinite: true,
+    fade: true, // Застосування ефекту зникання
+    autoplay: true,
+    autoplaySpeed: 3000, // Затримка між автоматичними переходами в мілісекундах
+    dots: true,
+    arrows: true,
+    swipe: true,
   };
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  slickInit(event: any) {
+    console.log('Slick carousel initialized:', event);
+  }
+
+  breakpoint(event: any) {
+    console.log('Breakpoint reached:', event);
+    if (event.currentBreakpoint === 'xs') {
+      this.slideConfig.slidesToShow = 1;
+    } else {
+      this.slideConfig.slidesToShow = 4;
+    }
+  }
+
+  afterChange(event: any) {
+    console.log('Slide changed:', event);
+  }
+
+  beforeChange(event: any) {
+    console.log('Before slide change:', event);
+  }
 }
